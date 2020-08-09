8c8b434224056d33e90a5bfe096785a3f67f282a
akirfin <roskaxposti@gmail.com>
2020-08-09 15:00:56 +0300

    install and startup

--- README.md
+++ README.md
@@ -6,14 +6,27 @@ Fun with python in krita
 Contains experimental example python plugins for Krita.
 May give some ideas how to do things.
 
-arc_welding_tool:
+## How to install
+    copy files and folders from ./pykrita to <your home folder>/krita/pykrita.
+
+    or change line in ./bin/install_plugins_and_start_krita.py
+    ```python
+    # Change this to something that starts up Krita
+    krita_app_path = r"<path to krita.exe or something similar>"
+    ```
+    then run install_plugins_and_start_krita.py using python.
+    (works nice when editing code & testing in Krita (my dev folder is elsewhere...))
+    (Krita should have some enviroment variable for plugins folders like
+    `KRITA_PLUGINS_PATH="/path/myplugins/:/more/plugins:/pushing/it"`)
+
+## arc_welding_tool
     How to add new tool to Krita.
     Add button to toolbar
     Show tool context gizmo in viewport.
 
-camera_layer:
+## camera_layer
     Custom layer node that pulls pixels from camera.
 
-fetch_gallery:
+## fetch_gallery
     Fetch image data from url to QImage,
     and then push Qimage to layer node.
---
+++ bin/install_plugins_and_start_krita.py
@@ -0,0 +1,52 @@
+"""
+
+Install plugins to Krita and run Krita with console.
+
+"""
+
+import sys
+import os
+from shutil import copy2
+from subprocess import Popen, PIPE
+
+# Change this to something that starts up Krita
+krita_app_path = r"D:\Program Files\Krita (x64)\bin\krita.exe"
+
+
+def get_krita_resource_dir():
+    platform_dirs = [
+            ('linux',  "~/.local/share/krita/pykrita"),
+            ('darwin', "~/.local/share/krita/pykrita"),
+            ('win32',  "~\AppData\Roaming\krita\pykrita")]
+    for prefix, dir in platform_dirs:
+        if sys.platform.startswith(prefix):
+            return os.path.expanduser(dir)
+    raise RuntimeError('Not supported platform, Krita resource dir can NOT be solved. (platform: {sys.platform!r})'.format(**locals()))
+
+
+def run_krita():
+    """
+    Pipe left for future redirecting...
+    """
+    process = Popen(krita_app_path, stdout=PIPE)
+    while process.poll() is None:
+        data = process.stdout.readline()
+        sys.stdout.write(data.decode('utf-8'))
+        sys.stdout.flush()
+    # read lingering data
+    data = process.stdout.read()
+    sys.stdout.write(data.decode('utf-8'))
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    this_dir = os.path.dirname(sys.argv[0])
+
+    pykrita_dir = os.path.join(this_dir, "..", "pykrita")
+    krita_resource_dir = get_krita_resource_dir()
+
+    for entry in pykrita_dir:
+        copy2(entry, krita_resource_dir)
+
+    # start krita
+    run_krita()
