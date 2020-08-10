8f99c901e3f7cf20499bf9f486a0d4c6b1282503
akirfin <roskaxposti@gmail.com>
2020-08-11 02:04:50 +0300

    html inside md containing img embeding base64 jpg with fallback relative path to ./resource/title_image.jpg note uses src and source at same time

--- bin/encode_image.py
+++ bin/encode_image.py
@@ -1,15 +1,18 @@
 import base64
 
 
-_img_template = '<img src="data:image/png;base64,{base64_data}" alt="title_image" width="512" height="109" title="Title"/>'
+
+_img_template = '<img src="{relative_path}" source="data:image/png;base64,{base64_data}" alt="title_image" width="512" height="109" title="Title"/>'
 
 def img_embad(img_path):
     base64_data = ""
+    relative_path = "./resources/title_image.jpg"
     with open(img_path, "rb") as f:
         base64_data = base64.b64encode(f.read()).decode('ascii')
     img_tag = _img_template.format(**locals())
     print(img_tag)
 
-
-if __name__ == "__main__":
-    img_embad(r"D:\projects\krita_python_fun\pykrita\arc_welding_tool\resources\title_image.jpg")
+if __name__ == '__main__':
+    #img_embad(r"D:\projects\krita_python_fun\pykrita\arc_welding_tool\resources\title_image.jpg")
+    #img_embad(r"D:\projects\krita_python_fun\pykrita\camera_layer\resources\title_image.jpg")
+    #img_embad(r"D:\projects\krita_python_fun\pykrita\fetch_gallery\resources\title_image.jpg")
